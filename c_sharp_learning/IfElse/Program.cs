0feed6bbd05436c72ea9f32fdb8e56b28385d5f5
sksy <rsposkrebyshev@gmail.com>
2026-04-22 00:07:30 +0300

    Added new projects

---
+++ c_sharp_learning/IfElse/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IfElse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Четное!");
+            }
+            else
+            {
+                Console.WriteLine("Нечетное!");
+            }
+
+            string check = null;
+
+            while ((check != "false") && (check != "true"))
+            {
+                Console.WriteLine("Возможные значения только true или false!");
+                check = Console.ReadLine();
+            }
+
+            string result = null;
+
+            if (bool.Parse(check))
+            {
+                result = "Проверка успешна!";
+            }
+            else
+            {
+                result = "Проверка ошибочка!";
+            }
+
+            Console.WriteLine(result);
+
+        }
+    }
+}
---
+++ c_sharp_learning/IncDecrOps/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IncDecrOps
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Инкремент - увеличение значения на единицу
+            // Декремент - уменьшение значения на единицу
+
+            int a = 0;
+            Console.WriteLine("Initial a = " + a);
+
+            a++;
+            Console.WriteLine("a++ = " + a);
+
+            a--;
+            Console.WriteLine("a-- = " + a);
+
+            //Операция постфиксного инкрмента (напр. a++) имеет наименьший приоритет
+            //Операция префиксного инкремента (напр. ++a) имеет наибольший приоритет
+
+            int b = 1;
+            int c = 1;
+
+            b = ++b * b; //данное выражение вернёт 4, так как сначала выполнит ++b (т.е. b = b+1), после чего b станет равно 2.
+                         //По итогу получаем b = 2 * 2
+
+            c = c++ * c; //данное выражение вернёт 2, так как сначала выполнится умножение (1*1), а уже потом прибавится 1.
+
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+
+        }
+    }
+}
