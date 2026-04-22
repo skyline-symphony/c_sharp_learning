1cb5dfbbcfaf65c19ef4a2b167ac9e9817564e40
sksy <rsposkrebyshev@gmail.com>
2026-04-22 22:28:29 +0300

    Added new project with SWITCH learning

---
+++ c_sharp_learning/SwitchOp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwitchOp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKey consoleKey = Console.ReadKey().Key; //Позволяет сохранить информацию, как конкретно клавиша была нажата на клавиатуре
+
+            switch (consoleKey)
+            {
+                default:
+                    Console.WriteLine("Такой клавиши нет в кейсах");
+                    break;
+                case ConsoleKey.A:
+                    Console.WriteLine("Была нажата клавиша A");
+                    break;
+                case ConsoleKey.Tab:
+                    Console.WriteLine("Была нажата клавиша Tab");
+                    break;
+            }
+
+            int a = int.Parse(Console.ReadLine());
+
+            switch (a) //В скобки передается значение, на основании которого будет выполняться тот или иной блок кода.
+                       //Например, число.
+            {
+                default: // Дефолтный блок на случай, если полученное на вход значение не соответствует ни одному кейсу.
+                    Console.WriteLine("Такого нет!");
+                    break;
+
+                case 1:
+                    Console.WriteLine("Введено число 1");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Введено число 2");
+                    break;
+            }
+
+
+
+        }
+    }
+}
